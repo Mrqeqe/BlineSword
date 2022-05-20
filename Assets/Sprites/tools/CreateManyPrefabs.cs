caacb650f49eafa2302cc85f37b35c67ff1379b5
mrqe <mrqe19035@gmail.com>
2022-05-20 12:05:02 +0800

    制作批量制作预制体工具，制作大量音符

---
+++ Assets/Sprites/tools/CreateManyPrefabs.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+public class CreateManyPrefabs 
+{
+    // Start is called before the first frame update
+    [MenuItem("CustomerTools/Create Prefab")]
+    static void CreatePrefab()
+    {
+        // Keep track of the currently selected GameObject(s)
+        GameObject[] objectArray = Selection.gameObjects;
+
+        // Loop through every GameObject in the array above
+        foreach (GameObject gameObject in objectArray)
+        {
+            // Create folder Prefabs and set the path as within the Prefabs folder,
+            // and name it as the GameObject's name with the .Prefab format
+            if (!Directory.Exists("Assets/Prefabs"))
+                AssetDatabase.CreateFolder("Assets", "Prefabs");
+            string localPath = "Assets/Prefabs/" + gameObject.name + ".prefab";
+
+            // Make sure the file name is unique, in case an existing Prefab has the same name.
+            localPath = AssetDatabase.GenerateUniqueAssetPath(localPath);
+
+            // Create the new Prefab and log whether Prefab was saved successfully.
+            bool prefabSuccess;
+            PrefabUtility.SaveAsPrefabAsset(gameObject, localPath, out prefabSuccess);
+            if (prefabSuccess == true)
+                Debug.Log("Prefab was saved successfully");
+            else
+                Debug.Log("Prefab failed to save" + prefabSuccess);
+        }
+    }
+
+}
